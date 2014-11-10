77a2252ec455901993ef938c8db41f4804ef9dc1
Kevin Mircovich <kmircovich1@gmail.com>
2014-11-10 00:49:13 -0500

    Start of 7.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  def show
+  	@user = User.find(params[:id])
+  	#debugger -This line is for Ruby version 4.2 (which I haven't upgraded to yet)
+  end
+
   def new
   end
 end
