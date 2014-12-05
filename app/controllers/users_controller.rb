1628f5449f93a77f3b9396076e4f434ec8e28029
Kevin Mircovich <kmircovich1@gmail.com>
2014-12-04 23:25:17 -0500

    Finnish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-    	#lame message here
+      flash[:success] = "Welcome " + @user.name + "."
+    	redirect_to @user
     else
       render 'new'
     end
