10848a43533a57251fa9a19c6112ffa9aab6e5c7
Kevin Mircovich <kmircovich1@gmail.com>
2014-12-13 19:04:03 -0500

    beginning of 8.2.4

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 SampleApp::Application.routes.draw do
 
+  get "sessions/new"
   root 'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
