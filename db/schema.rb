336f2b1a23c6c8e5d7a397c9e1a88f725efd9b9e
Kevin Mircovich <kmircovich1@gmail.com>
2015-01-03 23:21:06 -0500

    End of Chapter 9

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141218051101) do
-
-  create_table "microposts", force: true do |t|
-    t.text     "content"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20150104032118) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -27,6 +20,7 @@ ActiveRecord::Schema.define(version: 20141218051101) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
